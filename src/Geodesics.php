787fb5a9187440f2b13dfc2f85fea2060b7a088c
Alexander Getmansky <gaa@szrcai.ru>
2014-10-17 15:45:19 +0400

    Added sets and gets

--- src/Geodesics.php
+++ src/Geodesics.php
@@ -2,4 +2,40 @@
 namespace GetSky\Geodesics;
 
 class Geodesics {
+
+    private $first;
+    private $second;
+
+    public function distance(array $first, array $second) {
+        $distance = '100';
+        return $distance;
+    }
+
+    public function setFirstPoint($longitude, $latitude) {
+        if ($longitude > 180 || $longitude < -180) {
+            throw new \Exception("Incorrect value of longitude");
+        }
+        if ($latitude > 90 || $latitude < -90) {
+            throw new \Exception("Incorrect value of longitude");
+        }
+        $this->first = [$longitude, $latitude];
+    }
+
+    public function getFirstPoint() {
+        return [$this->first[0], $this->first[1]];
+    }
+
+    public function setSecondPoint($longitude, $latitude) {
+        if ($longitude > 180 || $longitude < -180) {
+            throw new \Exception("Incorrect value of longitude");
+        }
+        if ($latitude > 90 || $latitude < -90) {
+            throw new \Exception("Incorrect value of longitude");
+        }
+        $this->second = [$longitude, $latitude];
+    }
+
+    public function getSecondPoint() {
+        return [$this->second[0], $this->second[1]];
+    }
 }
--- tests/GeodesicsTest.php
+++ tests/GeodesicsTest.php
@@ -1,7 +1,76 @@
 <?php
+use GetSky\Geodesics\Geodesics;
+
 class GeodesicsTest extends PHPUnit_Framework_TestCase {
 
-    public function testCreateGeodesic()
-    {
+    /**
+     * @var Geodesics
+     */
+    private $geodesic;
+
+    public function testDistance() {
+        $distance = $this->geodesic->distance([20,39],[40,76]);
+        $this->assertSame('100', $distance);
+    }
+
+    /**
+     * @dataProvider providerPoint
+     */
+    public function testSetFirstPoint($x, $y) {
+        $this->geodesic->setFirstPoint($x, $y);
+        $this->assertSame([$x, $y], $this->geodesic->getFirstPoint());
+    }
+
+    /**
+     * @expectedException Exception
+     * @dataProvider providerPointException
+     */
+    public function testSetFirstPointException($x, $y) {
+        $this->geodesic->setFirstPoint($x, $y);
+    }
+
+    /**
+     * @dataProvider providerPoint
+     */
+    public function testSetSecondPoint($x, $y) {
+        $this->geodesic->setSecondPoint($x, $y);
+        $this->assertSame([$x, $y], $this->geodesic->getSecondPoint());
+    }
+
+    /**
+     * @expectedException Exception
+     * @dataProvider providerPointException
+     */
+    public function testSetSecondPointException($x, $y) {
+        $this->geodesic->setSecondPoint($x, $y);
+    }
+
+
+    public function providerPoint() {
+        return [
+            [20, 30],
+            [-45, -50],
+            [68, -24],
+            [-24, 24],
+            [180, 45],
+            [-180, 34]
+        ];
+    }
+
+    public function providerPointException() {
+        return [
+            [200, 20],
+            [181, -50],
+            [-181, -24],
+            [-200, 24],
+            [23, 100],
+            [-45, 91 ],
+            [134, -91],
+            [-27, -100]
+        ];
+    }
+
+    protected function setUp() {
+        $this->geodesic = new Geodesics();
     }
 }
